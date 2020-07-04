0f683136f65365e629da577c3d4797a02c83d2bf
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2020-07-04 10:44:59 -0400

    Use one const definition

--- utils/constants.go
+++ utils/constants.go
@@ -1,25 +1,27 @@
 package utils
 
 // Name is the name of the blockchain.
-const Name = "Dimosthenes"
+const (
+	Name = "Dimosthenes"
 
-// Version is the current version of the blockchain.
-const Version = "0.1.1"
+	// Version is the current version of the blockchain.
+	Version = "0.1.1"
 
-// DimosDir is the directory that the blockchain will live in.
-const DimosDir = ".dimos"
+	// DimosDir is the directory that the blockchain will live in.
+	DimosDir = ".dimos"
 
-// ChainDir is the directory where the blockchain database will live in.
-const ChainDir = "chain"
+	// ChainDir is the directory where the blockchain database will live in.
+	ChainDir = "chain"
 
-// Genesis is the name of the file that will contain the genesis hash.
-const Genesis = "genesis"
+	// Genesis is the name of the file that will contain the genesis hash.
+	Genesis = "genesis"
 
-// CurrentHash is the file that contains the current hash.
-const CurrentHash = "current_hash"
+	// CurrentHash is the file that contains the current hash.
+	CurrentHash = "current_hash"
 
-// MaxUnitSupply defines the maximum unit supply, which is 1 qadrillion.
-const MaxUnitSupply = 1e15
+	// MaxUnitSupply defines the maximum unit supply, which is 1 qadrillion.
+	MaxUnitSupply = 1e15
 
-// UnitsInCoin The amount of units in a coin are 100 million. As a result, we can have 10 million coins.
-const UnitsInCoin = 1e8
+	// UnitsInCoin The amount of units in a coin are 100 million. As a result, we can have 10 million coins.
+	UnitsInCoin = 1e8
+)
