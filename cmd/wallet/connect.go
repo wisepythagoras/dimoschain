954d52b79e9ef5c6f70e08aefe0de8a27114073f
Constantine <55108282+wisepythagoras@users.noreply.github.com>
2025-05-20 08:39:11 -0400

    Fix the wallet connection code

--- cmd/wallet/connect.go
+++ cmd/wallet/connect.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net"
 
-	"github.com/cossacklabs/themis/gothemis/session"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
@@ -25,6 +23,7 @@ type Client struct {
 	Address    string
 	Blockchain *core.Blockchain
 	randomness io.Reader
+	readWriter *bufio.ReadWriter
 }
 
 func (c *Client) Create() error {
@@ -45,18 +44,32 @@ func (c *Client) Create() error {
 		libp2p.Identity(prvKey),
 	)
 
-	rw, err := c.createInstanceAndConnect(ctx, h, c.Address)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.createInstanceAndConnect(ctx, h, c.Address)
 
 	if err != nil {
 		return err
 	}
 
 	// Create a thread to read and write data.
-	go writeData(rw)
-	go readData(rw)
+	go writeData(c.readWriter)
+	go readData(c.readWriter)
 
 	// Wait forever
 	select {}
+}
+
+func (c *Client) SendData(data []byte) error {
+	_, err := c.readWriter.Write(data)
+
+	if err != nil {
+		return err
+	}
+
+	return c.readWriter.Flush()
 }
 
 func (c *Client) createInstanceAndConnect(ctx context.Context, h host.Host, destination string) (*bufio.ReadWriter, error) {
@@ -92,97 +105,16 @@ func (c *Client) createInstanceAndConnect(ctx context.Context, h host.Host, dest
 	}
 
 	// Create a buffered stream so that read and writes are non-blocking.
-	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+	c.readWriter = bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
 
-	return rw, nil
+	return c.readWriter, nil
 }
-
-// ConnectToNode connects to the node with the specified IP address and port.
-/*func ConnectToNode(ip string, port int) (*Connection, error) {
-	// Validate the cnputs.
-	if !utils.IsIPAddressValid(ip) || port <= 0 || port > 65535 {
-		return nil, errors.New("Invalid IP address or port")
-	}
-
-	// The first thing we want to do is connect to the TCP port of the node.
-	conn, err := net.Dial("tcp", ip+":"+strconv.FormatInt(int64(port), 10))
-
-	if err != nil {
-		return nil, err
-	}
-
-	// Generate the keys necessary to communicate with the node.
-	clientKeyPair, err := keys.New(keys.TypeEC)
-
-	if err != nil {
-		return nil, err
-	}
-
-	pubKey := base64.StdEncoding.EncodeToString(clientKeyPair.Public.Value)
-
-	// This is what initiates a secure session.
-	secureSession, err := session.New([]byte(pubKey), clientKeyPair.Private, &proto.Callback{})
-
-	if err != nil {
-		return nil, err
-	}
-
-	buf, err := secureSession.ConnectRequest()
-
-	// The following for loop is taken from the `secure_session_client.go` example.
-	// TODO: Why is this necessary?
-	for {
-		_, err = conn.Write(buf)
-
-		if err != nil {
-			return nil, err
-		}
-
-		buf = make([]byte, 10240)
-		readBytes, err := conn.Read(buf)
-
-		if err != nil {
-			return nil, err
-		}
-
-		buffer, sendPeer, err := secureSession.Unwrap(buf[:readBytes])
-
-		if nil != err {
-			return nil, err
-		}
-
-		buf = buffer
-
-		if !sendPeer {
-			break
-		}
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	if err != nil {
-		fmt.Println("raw message error")
-		return nil, err
-	}
-
-	connection := &Connection{
-		secureSession: secureSession,
-		connection:    conn,
-		ip:            ip,
-		port:          port,
-	}
-
-	return connection, nil
-}*/
 
 // Connection is the type that handles a connection with a node.
 type Connection struct {
-	ip            string
-	port          int
-	secureSession *session.SecureSession
-	connection    net.Conn
+	ip     string
+	port   int
+	Client *Client
 }
 
 // SendCommand sends a command to
@@ -198,15 +130,7 @@ func (c *Connection) SendCommand(command int, payload interface{}) bool {
 		return false
 	}
 
-	// This encrypts the message that's sent to the node.
-	buf, err := c.secureSession.Wrap(packed)
-
-	if err != nil {
-		return false
-	}
-
-	// Now we send the encrypted message over to the node.
-	_, err = c.connection.Write(buf)
+	err = c.Client.SendData(packed)
 
 	if err != nil {
 		return false
