7d6b706941df93fdedf6dcc92f684f10f952f670
Constantine <55108282+wisepythagoras@users.noreply.github.com>
2025-05-20 22:41:14 -0400

    Add utils

---
+++ cmd/wallet/utils.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, _ := rw.ReadString('\n')
+
+		if str == "" {
+			return
+		}
+
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		rw.Flush()
+	}
+}
